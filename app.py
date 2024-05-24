d9b351d249991f550579314e9f2da86146859315
usman3721 <96980113+usman3721@users.noreply.github.com>
2024-05-24 12:29:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+    st.session_state.messages = []
 
 time.sleep(0.1)
 
